04b1ca5bb32f66b56b48ca25a2765a5746f9b361
Andranik Torgomyan <andranik.torgomyan1998@gmail.com>
2022-04-28 02:38:18 +0400

    Start project Sys

---
+++ js/script.js
@@ -0,0 +1,65 @@
+const { active } = {
+    active: 'active'
+}
+const mobileMenu = $('.mobile-menu-board');
+const _menu = $('main .menu');
+
+// -------------- OPEN CLOSE MENU ------------------
+
+const menu = $('main .menu .menu-item .menu-name');
+const menuItems = $('main .menu .menu-item');
+
+menu.on('click', function (){
+    const thisParent = $(this).parent();
+    if (thisParent.hasClass(active)){
+        thisParent.removeClass(active);
+    } else {
+        menuItems.removeClass(active);
+        thisParent.addClass(active);
+    }
+})
+
+// -------------- OPEN CLOSE MENU ------------------
+
+
+
+
+// -------------- FOR DEFAULT INPUT FUNCTION -------------------
+
+
+const defaultInput = $('.def-input input');
+const inpLabel = $('.def-input');
+
+defaultInput.on('blur', function (){
+    inpLabel.map((index, e) => {
+        if($(e).children('input').val() === ''){
+            $(e).removeClass(active);
+        }
+    })
+})
+
+defaultInput.on('focus', function (){
+    $(this).parent().addClass(active);
+})
+
+
+// -------------- FOR DEFAULT INPUT FUNCTION -------------------
+
+
+const menuCatItems = $('main .menu .menu-item .menu-category-block .category-item');
+menuCatItems.on('click', function (){
+    menuCatItems.removeClass(active);
+    _menu.removeClass(active);
+    mobileMenu.removeClass(active);
+    $(this).addClass(active);
+})
+
+mobileMenu.on('click', function (){
+    if(_menu.hasClass(active)){
+        mobileMenu.removeClass(active);
+        _menu.removeClass(active);
+    } else {
+        mobileMenu.addClass(active);
+        _menu.addClass(active);
+    }
+})
